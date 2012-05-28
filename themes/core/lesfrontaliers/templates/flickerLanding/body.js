1be4f4f2b1c709f64b09e64f2382168d8b7d6301
vincent.lark@gmail.com <vincent.lark@gmail.com>
2012-05-28 19:29:34 +0200

    FLicker feed test

---
+++ themes/core/lesfrontaliers/templates/flickerLanding/body.js
@@ -0,0 +1,45 @@
+/**
+ * Additional content section / block functions for body.
+ */
+
+var rootpath = process.cwd() + '/',
+  path = require('path'),
+  calipso = require(path.join(rootpath, 'lib/calipso')),
+  Query = calipso.lib.mongoose.Query;
+
+exports = module.exports = function(req, options, callback) {
+
+  /**
+   *  Get additional content for blocks in the template
+   */
+  calipso.lib.step(
+
+  function getContentList() {
+
+    options.getContent(req, 'github-header-text', this.parallel());
+
+    // Create a query and retrieve the content, has pager support, using req.moduleParams
+    // But you can override on an individual query by setting in the options (second param)
+    var query = new Query({
+      'contentType': 'Flicker Feed'
+    });
+
+    options.getContentList({contentType: 'Flicker Feed'}, {
+      req: req,
+      sortBy: 'published,desc',
+      limit: 30
+    }, this.parallel());
+
+  }, function done(err, header, output) {
+
+    console.dir(output);
+
+    callback(err,{
+      header: header,
+      contents: output.contents,
+      pager: output.pager
+    });
+
+  });
+
+};
